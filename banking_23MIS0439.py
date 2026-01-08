6852d7357392f98bf7ff5276bf701f62e9f3c3f6
priyasrij30-hub <priyasrij30@gmail.com>
2026-01-08 22:20:45 +0530

    Added withdraw amount and prevents overdraft

---
+++ banking_23MIS0439.py
@@ -0,0 +1,24 @@
+balance = 1000
+
+while True:
+    print("\n1.Check Balance  2.Deposit  3.Withdraw  4.Exit")
+    choice = input("Choice: ")
+
+    if choice == "1":
+        print("Balance:", balance)
+    elif choice == "2":
+        amt = int(input("Enter amount: "))
+        balance += amt
+        print("Amount deposited")
+    elif choice == "3":
+        amt = int(input("Enter amount: "))
+        if amt <= balance:
+            balance -= amt
+            print("Withdrawal successful")
+        else:
+            print("Insufficient balance")
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice")
+
